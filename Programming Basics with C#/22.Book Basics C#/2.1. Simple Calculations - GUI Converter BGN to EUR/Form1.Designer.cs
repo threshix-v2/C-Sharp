de1c0d3bacbe8b8030a368eaf31d97464d219731
Milen Stoychev <114076230+threshix-v2@users.noreply.github.com>
2022-10-21 18:24:29 +0300

    .

---
+++ Programming Basics with C#/22.Book Basics C#/2.1. Simple Calculations - GUI Converter BGN to EUR/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace _2._1._Simple_Calculations___GUI_Converter_BGN_to_EUR
+{
+    partial class FormConverter
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownAmount = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxCurrency = new System.Windows.Forms.ComboBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownAmount
+            // 
+            this.numericUpDownAmount.DecimalPlaces = 2;
+            this.numericUpDownAmount.Location = new System.Drawing.Point(44, 41);
+            this.numericUpDownAmount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownAmount.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.Name = "numericUpDownAmount";
+            this.numericUpDownAmount.Size = new System.Drawing.Size(154, 29);
+            this.numericUpDownAmount.TabIndex = 0;
+            this.numericUpDownAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownAmount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.ValueChanged += new System.EventHandler(this.numericUpDownAmount_ValueChanged);
+            // 
+            // comboBoxCurrency
+            // 
+            this.comboBoxCurrency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCurrency.FormattingEnabled = true;
+            this.comboBoxCurrency.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "GBP"});
+            this.comboBoxCurrency.Location = new System.Drawing.Point(277, 41);
+            this.comboBoxCurrency.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxCurrency.Name = "comboBoxCurrency";
+            this.comboBoxCurrency.Size = new System.Drawing.Size(154, 29);
+            this.comboBoxCurrency.TabIndex = 1;
+            this.comboBoxCurrency.SelectedIndexChanged += new System.EventHandler(this.comboBoxCurrency_SelectedIndexChanged);
+            // 
+            // labelResult
+            // 
+            this.labelResult.BackColor = System.Drawing.Color.PaleGreen;
+            this.labelResult.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelResult.Location = new System.Drawing.Point(44, 98);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(387, 31);
+            this.labelResult.TabIndex = 2;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "BGN ->";
+            // 
+            // FormConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 200);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.comboBoxCurrency);
+            this.Controls.Add(this.numericUpDownAmount);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormConverter";
+            this.Text = "Currency Converter";
+            this.Load += new System.EventHandler(this.FormConverter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownAmount;
+        private System.Windows.Forms.ComboBox comboBoxCurrency;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label label2;
+    }
+}
